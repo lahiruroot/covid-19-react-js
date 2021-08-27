fc0da022962fe6982a45f097610b7bc135d21a1e
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2021-08-27 23:44:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Data from './Data/Data'
 import Navbar from './Navbar/Navbar'
+import Cases from './Cards/Cases'
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Data/>
+      <Cases/>
     </div>
   )
 }

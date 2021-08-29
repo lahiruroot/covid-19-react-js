05781f32093a02c630731332dc6e7e61c7173a80
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2021-08-29 19:24:34 +0530

    Global

--- src/Globle/Globle.js
+++ src/Globle/Globle.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import axios from 'axios'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,38 +21,65 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Globle() {
     const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+    const [loading, setloading] = useState(false);
+    const [Total, setTotal] = useState(null);
+    const [NewRecovered,setNewrecover] = useState(null);
+    const [Deths, setDeths] = useState(null);
+    const [Recover, setRecover] = useState(null);
+    const [Cases, setCases] = useState(null);
+    const [Time, setTime] = useState(null);
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "https://api.covid19api.com/summary",
+        }).then((Response) => {
+            setloading(true);
+            setCases(Response.data.Global.NewConfirmed);
+            setTotal(Response.data.Global.TotalConfirmed);
+            setNewrecover(Response.data.Global.NewRecovered)
+            setDeths(Response.data.Global.NewDeaths);
+            setRecover(Response.data.Global.TotalDeaths);
+            setTime(Response.data.Global.Date);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, [])
+
 
     return (
         <React.Fragment>
             <CssBaseline />
-            <Container fixed>
-                <h1>Globle Cases</h1>
-                <div className={classes.root}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>ghuisdhihui</Paper>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Paper className={classes.paper}>gsdgsidhgsu</Paper>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Paper className={classes.paper}>xs=12 sm=6</Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                        </Grid>
-                    </Grid>
+            <Container maxWidth="fixed">
+                <h1>Global Cases</h1>
+                <p>Last updated at: {loading ? Time : <div>Getting Data...</div>}</p>
+                <div style={{ display: 'flex', justifyContent: "center", alignItems: "center" }}>
+                    <div style={{ flexGrow: 1 }}>
+                        <div className={classes.root}>
+                            <Grid container spacing={1}>
+                                <Grid item xs={12}>
+                                    <Paper className={classes.paper}><h2>New Cases:</h2><h1>{loading ? Cases : <div>Getting Data...</div>}</h1></Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper className={classes.paper}>Total Cases : {loading ? Total : <p>Getting Data...</p>}</Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper className={classes.paper}>New recover:{loading ? NewRecovered : <p>Getting Data...</p>}</Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper className={classes.paper}>Total Deaths : {loading ? Recover : <p>Getting Data...</p>}</Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper className={classes.paper}>New Deaths : {loading ? Deths : <p>Getting Data...</p>}</Paper>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
                 </div>
             </Container>
+            <br></br>
         </React.Fragment>
-    )
+    );
 }
+
